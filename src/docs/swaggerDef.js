6d7fcdeb6739c1ae04eec5517dd40e6b7bfabd09
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-11-07 13:07:02 -0500

    removed port from servers url of swagger def

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,5 +1,5 @@
 const { version } = require('../../package.json');
-const config = require('../config/config');
+// const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
@@ -13,7 +13,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `https://openspec-userauth.herokuapp.com:3000/v1`,
+      url: `https://openspec-userauth.herokuapp.com/v1`,
     },
   ],
 };
